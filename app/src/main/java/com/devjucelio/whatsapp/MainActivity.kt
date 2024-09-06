cc115aad9c6032e8495eaf13be522921c3e8aaa3
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2024-09-06 16:23:50 -0300

    PerfilActivity.kt criada

--- app/src/main/java/com/devjucelio/whatsapp/MainActivity.kt
+++ app/src/main/java/com/devjucelio/whatsapp/MainActivity.kt
@@ -1,13 +1,83 @@
 package com.devjucelio.whatsapp
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.MenuProvider
+import com.devjucelio.whatsapp.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate( layoutInflater )
+    }
+
+    //Firebase
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView( binding.root )
+        inicializarToolbar()
 
     }
+
+    private fun inicializarToolbar() {
+
+        val toolbar = binding.includeMainToolbar.tbPrincipal
+        setSupportActionBar( toolbar )
+        supportActionBar?.apply {
+            title = "WhatsApp"
+        }
+
+        addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.menu_principal, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+
+                    when( menuItem.itemId ){
+                        R.id.item_perfil -> {
+                            startActivity(
+                                Intent(applicationContext, PerfilActivity::class.java)
+                            )
+                        }
+                        R.id.item_sair -> {
+                            deslogarUsuario()
+                        }
+                    }
+                    return true
+                }
+
+            }
+        )
+
+    }
+
+    private fun deslogarUsuario() {
+
+        AlertDialog.Builder(this)
+            .setTitle("Deslogar")
+            .setMessage("Deseja realmente sair?")
+            .setNegativeButton("Não"){dialog, posicao -> }
+            .setPositiveButton("Sim"){dialog, posicao ->
+                firebaseAuth.signOut()
+                startActivity(
+                    Intent(applicationContext, LoginActivity::class.java)
+                )
+            }
+            .create()
+            .show()
+
+    }
+
 }
